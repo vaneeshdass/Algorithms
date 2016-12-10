974f8ada3a6488888bed169bc902c1e98f7a928f
vanee <vanee@Vaneesh-ProBook>
2016-12-10 15:09:55 +0530

    second commit added new line

--- Algos/src/algorithms/code/Test.java
+++ Algos/src/algorithms/code/Test.java
@@ -3,7 +3,7 @@ package algorithms.code;
 public class Test {
 	 public static void main(String[] args)
 	 {
-	     System.out.println("Hello change ");
+	     System.out.println("i added new line ");
 	    
 	 }
 }
